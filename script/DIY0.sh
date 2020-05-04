1047946c8b28cf540b81a0c32d50eb54794a4e6a
molun <xinhiman@126.com>
2020-05-04 20:39:35 +0800

    update

---
+++ script/DIY0.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+# This is free software, lisence use MIT.
+# Copyright (C) https://github.com/molun
+
+# Define My Package
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
+git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
+git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
