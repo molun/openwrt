7fee802cffd48a86e6c9795a409d7aa679463efe
molun <xinhiman@126.com>
2020-05-06 18:52:28 +0800

    update

--- script/DIY0.sh
+++ script/DIY0.sh
@@ -4,5 +4,6 @@
 
 # Define My Package
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
-git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
 git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
+
+echo "src-git molun https://github.com/fw876/helloworld" >> feeds.conf.default
