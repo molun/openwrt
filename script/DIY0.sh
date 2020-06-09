b700f4859ac2869685e86603e839ecae9fae75d8
molun <xinhiman@126.com>
2020-06-09 17:51:12 +0800

    update

--- script/DIY0.sh
+++ script/DIY0.sh
@@ -5,6 +5,6 @@
 # Define My Package
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
-#git clone https://github.com/vernesong/luci-app-clash.git                                package/molun/luci-app-clash
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/molun/luci-app-adguardhome
+#git clone https://github.com/vernesong/luci-app-clash.git package/molun/luci-app-clash
 #echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
