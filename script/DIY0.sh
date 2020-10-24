f33f4e4fa95a99c98a741480a5807dd94064a6cb
molun <xinhiman@126.com>
2020-10-24 14:51:05 +0800

    update

--- script/DIY0.sh
+++ script/DIY0.sh
@@ -1,8 +1,11 @@
 #/bin/bash
 # This is free software, lisence use MIT.
-# Copyright (C) https://github.com/yfdoor
+# Copyright (C) https://github.com/molun
 
 # Define My Package
-git clone https://github.com/yfdoor/OpenWrt-AdGuardHome.git                         package/yfdoor/adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/yfdoor/luci-app-adguardhome
-git clone https://github.com/fw876/helloworld.git                                   package/yfdoor/luci-app-ssr-plus
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+git clone https://github.com/kenzok8/small.git package/small
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/molun/luci-app-adguardhome
+#git clone https://github.com/vernesong/luci-app-clash.git package/molun/luci-app-clash
+#git clone https://github.com/fw876/helloworld.git package/molun/luci-app-ssr-plus
+#git clone https://github.com/yfdoor/OpenWrt-AdGuardHome.git package/yfdoor/adguardhome
--- script/DIY1.sh
+++ script/DIY1.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 # This is free software, lisence use MIT.
-# Copyright (C) https://github.com/yfdoor
+# Copyright (C) https://github.com/molun
 
 # Define IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Define Default
 cat > package/lean/default-settings/files/zzz-default-settings <<-EOF
@@ -16,7 +16,7 @@ cat > package/lean/default-settings/files/zzz-default-settings <<-EOF
     # set distfeeds
     cp /etc/opkg/distfeeds.conf /etc/opkg/distfeeds.conf_BK
     sed -i 's#http://downloads.openwrt.org#http://mirrors.tuna.tsinghua.edu.cn/openwrt#g' /etc/opkg/distfeeds.conf
-    sed -i '/yfdoor/d' /etc/opkg/distfeeds.conf
+    sed -i '/molun/d' /etc/opkg/distfeeds.conf
     
     # set firewall
     sed -i '/REDIRECT --to-ports 53/d' /etc/firewall.user
@@ -26,9 +26,7 @@ cat > package/lean/default-settings/files/zzz-default-settings <<-EOF
     # Others
     sed -i '/log-facility/d' /etc/dnsmasq.conf
     echo "log-facility=/dev/null" >> /etc/dnsmasq.conf
-
     sed -i 's/cbi.submit\"] = true/cbi.submit\"] = \"1\"/g' /usr/lib/lua/luci/dispatcher.lua
-
     echo 'hsts=0' > /root/.wgetrc
     
     rm -rf /tmp/luci*
